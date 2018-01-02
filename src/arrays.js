b1122be403d82ec91a71ef11976d9937d8de319b
Cintia Higashi <cintiamh@gmail.com>
2018-01-02 00:01:36 -0800

    some notes

--- src/arrays.js
+++ src/arrays.js
@@ -1,3 +1,26 @@
 export default () => {
   console.log('Arrays');
+
+  var fibonacci = [];
+
+  fibonacci[0] = 1;
+  fibonacci[1] = 1;
+
+  for (var i = 2; i < 20; i++) {
+    fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+  }
+  for (var j = 0; j < fibonacci.length; j++) {
+    console.log(fibonacci[j]);
+  }
+
+  var numbers = [0, 1, 2, 3, 4, 5];
+  numbers[numbers.length] = 6;
+
+  numbers.push(7);
+  numbers.push(8, 9);
+
+  numbers.unshift(-1);
+  numbers.unshift(-3, -2);
+
+  console.log(numbers);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const PATHS = {
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const commonConfig = {
   entry: {
     src: PATHS.src
   },
@@ -20,3 +20,42 @@ module.exports = {
     }),
   ],
 };
+
+const productionConfig = () => commonConfig;
+
+const developmentConfig = () => {
+  const config = {
+    devServer: {
+      historyApiFallback: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT,
+      overlay: {
+        errors: true,
+        warnings: true
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader'
+        }
+      ]
+    }
+  };
+
+  return Object.assign(
+    {},
+    commonConfig,
+    config
+  );
+};
+
+module.exports = (env) => {
+  if (env === 'production') {
+    return productionConfig();
+  }
+  return developmentConfig();
+}
